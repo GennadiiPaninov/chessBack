09c3d6c29212dc6b0e5a1f6905bd6044053b99f4
Gennadij Lobotskij <utyf185@mail.ru>
2025-02-19 11:04:00 +0300

    Init lib

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 const port = process.env.PORT || 8080;
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false } // Нужно для Railway
+// });
 app.get('/', (req, res) => res.send('Express on V'));
 
 
