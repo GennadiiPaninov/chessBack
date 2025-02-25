00c934607609b6239245521d284309162ae3c038
Gennadij Lobotskij <utyf185@mail.ru>
2025-02-25 14:51:49 +0300

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  console.log('Starting NestJS application...');
+  await app.init();
+  console.log('NestJS application initialized');
+  return server;
 }
-bootstrap();
+
+// Экспорт для Vercel
+export default async (req, res) => {
+  const handler = await bootstrap();
+  handler(req, res);
+};
